f26e57f6a9231d8ea50be4b0db9f1c7cbeff006b
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2023-09-08 08:52:16 -0400

    feat: UILoading

---
+++ components/UILoading/UILoading.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="progress-container">
+    <v-progress-circular indeterminate :size="50" :width="4" />
+  </div>
+</template>
+
+<style scoped>
+.progress-container {
+  height: 100%;
+  width: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
