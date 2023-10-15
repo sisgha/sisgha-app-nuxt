25f2ae1d4ad75937e299c879b81a88b09ff83df6
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2023-10-14 20:22:54 -0400

    feat: PageDashboardCursosContentSearchFields

--- components/PageDashboardCursos/PageDashboardCursosContentSearch.vue
+++ components/PageDashboardCursos/PageDashboardCursosContentSearch.vue
@@ -1,29 +1,6 @@
 <template>
   <div>
-    <div class="search-fields">
-      <VRow class="align-center">
-        <VCol cols="9">
-          <PageDashboardCursosContentSearchFormFieldQuery />
-        </VCol>
-
-        <VCol cols="3">
-          <PageDashboardCursosContentSearchFormFieldDeleteSituation />
-        </VCol>
-      </VRow>
-
-    </div>
-
+    <PageDashboardCursosContentSearchFields />
     <PageDashboardCursosContentSearchResultTable />
   </div>
 </template>
-
-<style scoped>
-.search-fields {
-  margin: 2rem 0;
-  display: flex;
-
-  flex-direction: column;
-
-  gap: 1.5rem;
-}
-</style>
---
+++ components/PageDashboardCursos/PageDashboardCursosContentSearchFields.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="search-fields">
+    <PageDashboardCursosContentSearchFormFieldQuery class="search-field-query" />
+    <PageDashboardCursosContentSearchFormFieldDeleteSituation class="search-field-delete-situation" />
+  </div>
+</template>
+
+<style scoped>
+.search-fields {
+  margin: 2rem 0;
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+
+  gap: 1.5rem;
+}
+
+.search-field-query {
+  flex: 9 1 10rem;
+}
+
+.search-field-delete-situation {
+  flex: 1 0 8rem;
+}
+</style>
